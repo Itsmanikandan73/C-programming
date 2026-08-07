ebf27e891bf8e185b1bc5a24b5e57ca53e6a2eee
itsmanikandan73 <soulquiet8@gmail.com>
2026-08-07 14:17:35 +0530

    adding exit_codes.c program

---
+++ step1/exit_codes.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>     // Requred for EXIT_SUCCESS and EXIT_FAILURE
+
+int main(void) {
+    fprintf(stdout,"Executing program logic...\n");
+    // standared way to return a success status to the OS
+    return EXIT_SUCCESS;
+}
