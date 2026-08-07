aae113abf4d392e2b648c00c1bbec601d3c42e1d
itsmanikandan73 <soulquiet8@gmail.com>
2026-08-07 14:19:38 +0530

    adding output_formatting.c program

---
+++ step1/output_formatting.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Iteam\t\tQuantity\tStatus\n");
+    printf("--------------------------------------\n");
+    printf("Compiler\t1\t\t\"Active\"\n");
+    printf("Debugger\t1\t\t\"Ready\"\n");
+    printf("GDB\t\t1\t\t\"Using\"\n");
+
+    return 0;
+}
